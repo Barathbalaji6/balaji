664d3f94da1a15ff103cd57d8e4d180a58af77de
Barathbalaji6 <barathbalaji6@gmail.com>
2017-06-28 15:30:06 +0530

    Create evennumbers.c

---
+++ evennumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int start,stop,i;
+printf("enter the starting and ending numbers between to find the evn series");
+scanf("%d%d",&start,&stop);
+for(i=start;i<=;i++)
+{
+if(i%2==0)
+printf("\n %d",i);
+}
+printf("the above is the even series");
+return 0;
+}
