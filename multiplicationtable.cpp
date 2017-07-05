5d94d58ac70a421ec5ac36960db68a28e45d974b
Barathbalaji6 <barathbalaji6@gmail.com>
2017-07-05 14:59:24 +0530

    Create multiplicationtable.cpp

---
+++ multiplicationtable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    int x=0,y=0;
+    cout<<"Enter the Number";
+    cin>>x;
+    for(int i=0;i<=10;i++)
+    {  y=x*i;
+      cout<<x<<" X "<<i<<" = "<<y<<endl;
+      
+      
+    }
+   return 0;
+}
